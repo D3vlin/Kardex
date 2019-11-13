d2cc24c44ec27b59ff4d512a1ca19f813bb1cbf7
D3vlin <alexisdq16@gmail.com>
2019-11-13 17:50:23 -0500

    Se añaden registros a la tabla de tiendas y se añade registro de un nuevo usuario

--- HulkStore/dataDump.sql
+++ HulkStore/dataDump.sql
@@ -1,5 +1,9 @@
 /* DATA DUMP */
 
+-- Stores
+INSERT INTO store VALUES(DEFAULT, 'MaxPower', 'Carrera 20', 1);
+INSERT INTO store VALUES(DEFAULT, 'IronShop', 'Avenida 3', 1);
+
 -- Unities
 INSERT INTO UNITY VALUES(DEFAULT, 'unidades', 1);
 INSERT INTO UNITY VALUES(DEFAULT, 'cajas', 1);
@@ -10,5 +14,6 @@ INSERT INTO UNITY VALUES(DEFAULT, 'comics', 1);
 -- Documents
 INSERT INTO DOCUMENT VALUES(DEFAULT, 'Factura', 1);
 
--- Creation of admin
-INSERT INTO USERS VALUES(DEFAULT, 'admin', MD5('admin'), '00000000', 'Admin', 'ADMIN', 1, 1);
+-- Creation of users
+INSERT INTO USERS VALUES(DEFAULT, 'admin', MD5('admin'), '00000000', 'Admin', 'Master', 1, 1);
+INSERT INTO USERS VALUES(DEFAULT, 'user', MD5('user'), '11111111', 'User', 'Standar', 0, 1);
